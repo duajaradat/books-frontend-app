5e69a8741fdbf4f327e974348c31c66e9b5b611e
duajaradat <duajaradat164@gmail.com>
2021-08-22 16:10:44 +0300

    lab011 auth0

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
 import React from 'react';
 import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
+// import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './Footer';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile"
+import MyFavoriteBooks from "./BestBooks"
+import { withAuth0 } from '@auth0/auth0-react';
+
 
 class App extends React.Component {
 
   render() {
     console.log('app', this.props);
-    return(
+    return (
       <>
         <Router>
-          <IsLoadingAndError>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-              </Route>
+          {/* <IsLoadingAndError> */}
+          <Header />
+          <Switch>
+            <Route exact path="/" >
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+              {this.props.auth0.isAuthenticated ? <MyFavoriteBooks /> : <Login />}
+            </Route>
+
+            <Route exact path="/profile">
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-            </Switch>
-            <Footer />
-          </IsLoadingAndError>
+              <Profile />
+            </Route>
+
+
+            {/* <Route exact path='/'>
+              {isAuthenticated ? <Redirect to="/home" /> : <LandingPage />}
+            </Route> */}
+
+
+
+          </Switch>
+          <Footer />
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     );
   }
 }
 
-export default App;
+export default withAuth0(App);
