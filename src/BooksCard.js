ad60863941c0adb0d468a4540c8557cbfface3a1
duajaradat <duajaradat164@gmail.com>
2021-08-28 22:01:56 +0300

    update book data lab14 part 1

--- src/BooksCard.js
+++ src/BooksCard.js
@@ -7,16 +7,24 @@ export class BooksCard extends Component {
     deleteBook = () => {
         this.props.delete(this.props.book._id)
     };
+    updateBook = () => {
+        this.props.update(this.props.book._id)
+    };
+
     render() {
         return (
             <div>
 
-                <Card style={{ width: '18rem' }} border="secondary">
+                <Card style={{ width: '20rem' }} border="secondary">
                     <Card.Body>
                         <Card.Title>{this.props.book.title}</Card.Title>
                         <Card.Text>{this.props.book.description}</Card.Text>
                     </Card.Body>
-                    <Button variant="secondary" onClick={this.deleteBook}>Delete Book</Button>
+
+                    <Button variant="secondary" size="m" onClick={this.deleteBook}>Delete Book</Button>
+                    <Button variant="secondary" size="m" onClick={this.updateBook}>Update Book</Button>
+
+
                 </Card>
 
             </div>
---
+++ src/UpdateBooks.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Modal, Form, Button } from 'react-bootstrap'
+
+export class UpdateBooks extends Component {
+
+    render() {
+        // const { user } = this.props and in defaultvalue={user}
+
+        return (
+            <div>
+                <Modal show={this.props.updatemodal} onHide={this.props.close}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Update Your Book</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+
+                        < Form onSubmit={this.props.updateInfo}>
+                            <Form.Group className="mb-3"  >
+                                <Form.Control type="email" defaultValue={this.props.user.email} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" >
+                                <Form.Control type="text" name="title" rows={3} defaultValue={this.props.selectedBook.title} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" >
+                                <Form.Control as="textarea" name="description" rows={3} defaultValue={this.props.selectedBook.description} />
+                            </Form.Group>
+                            <Button variant="primary" type="submit" onClick={this.props.close} >UPDATE</Button>
+                        </Form >
+
+                    </Modal.Body>
+
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default UpdateBooks
+
+
+
+
+
+
